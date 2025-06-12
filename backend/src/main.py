3541a7fc969de3249ac227b112ed9739b1ae1c35
el mrabti yahya <yahya181elmrabti@gmail.com>
2025-06-13 00:59:04 +0100

    feat: converting the file to chunks, getting its embeddings & storing it in chromadb. added to the upload route

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
-from utils import parser
+from sentence_transformers import SentenceTransformer
+from utils.parser import parse_file
+from utils.checksum import calculate_checksum
+from utils.chunk_text import chunk_text
+from db.store_vector import store_chunks
+from db.store_vector import existing
+
 
 app = FastAPI()
 
@@ -8,15 +14,29 @@ app = FastAPI()
 async def upload_file(file: UploadFile = File(...)):
     filename = file.filename
     contents = await file.read()
+    checksum = calculate_checksum(contents)
 
+    if existing(checksum=checksum):
+        return {"message": "File already uploaded", "filename": file.filename}
+    
     try:
-        text = parser.parse_file(contents, filename)
+        text = parse_file(contents, filename)
     except ValueError as e:
         return JSONResponse(content={"error": str(e)}, status_code=400)
 
-    # Just return preview for now
-    return JSONResponse(content={
-        "filename": filename,
-        "length": len(text),
-        "preview": text[:300]  # show first 300 characters
-    })
+    model = SentenceTransformer("BAAI/bge-m3")
+    chunks = chunk_text(text)
+    embeddings = model.encode(chunks).tolist()
+
+    success = store_chunks(
+        chunks=chunks,
+        embeddings=embeddings,
+        metadata={"filename": file.filename, "checksum": checksum}
+    )
+
+    return {
+        "message": "File processed and stored",
+        "filename": file.filename,
+        "checksum": checksum,
+        "chunks": len(chunks)
+    }
