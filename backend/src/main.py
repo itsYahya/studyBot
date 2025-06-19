778841b53e1a5d35cb273a32eb7f33cc5eb307d4
el mrabti yahya <yahya181elmrabti@gmail.com>
2025-06-19 19:59:21 +0100

    feat: adding a chat_history rout with pagenation

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI, UploadFile, File
 from fastapi.responses import JSONResponse
 from sentence_transformers import SentenceTransformer
+from datetime import datetime
 from utils.parser import parse_file
 from utils.checksum import calculate_checksum
 from utils.chunk_text import chunk_text
@@ -12,6 +13,12 @@ from db.store_vector import collection
 from tutor.question import ask
 from tutor.quiz import generate_quiz
 from models.ask_request import AskRequest
+from models.ask_request import ChatMsg
+from models.pagination_request import PaginationRequest
+from db.mongodb import chat_history
+from db.mongodb import questions
+from utils.serialize_item import serialize_item
+from fastapi.middleware.cors import CORSMiddleware
 
 model = SentenceTransformer("BAAI/bge-m3")
 
@@ -60,11 +67,33 @@ def list_uploaded_files():
     return files
 
 @app.post("/ask")
-def ask_question(data: AskRequest):
-    answer = ask(model, collection, data.checksum, data.question)
-    return answer
+async def ask_question(data: AskRequest):
+    result = await chat_history.insert_one(ChatMsg(right = True, message = data.question, date = str(datetime.now())).dict())
+    id = await questions.insert_one(data.dict())
+    #answer = ask(model, collection, data.checksum, data.question)
+    result = await chat_history.insert_one(ChatMsg(right = False, message = "this is the bot answer!", date = str(datetime.now())).dict())
+    return {"answer": str(result.inserted_id)}
 
 @app.post("/quiz/{checksum}")
 def quiz(checksum: str):
     res = generate_quiz(checksum, collection)
     return res
+
+
+@app.post("/chat_history/")
+async def get_items(payload: PaginationRequest):
+    page = max(payload.page, 1)
+    limit = min(max(payload.limit, 1), 20)
+
+    skip = (page - 1) * limit
+    cursor = chat_history.find({}).skip(skip).limit(limit)
+
+    items = [serialize_item(doc) async for doc in cursor]
+    total = await chat_history.count_documents({})
+
+    return {
+        "page": page,
+        "limit": limit,
+        "total": total,
+        "items": items
+    }
